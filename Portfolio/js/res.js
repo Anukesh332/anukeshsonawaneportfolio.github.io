d6d023d7f7166210557792bf9e7a32b689d80887
Anukesh332 <110771630+Anukesh332@users.noreply.github.com>
2022-08-07 18:18:36 +0530

    mywebsite

---
+++ Portfolio/js/res.js
@@ -0,0 +1,14 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.navlist')
+rightnav = document.querySelector('.rightnav')
+
+
+
+
+burger.addEventListener('click', ()=>{
+    rightnav.classList.toggle('v-class-res');
+    navlist.classList.toggle('v-class-res');
+    navbar.classList.toggle('h-nav-res')
+})
+
